278051f3fddd72c945acf8744276d7f0fac14899
Vanja Gavric <vanja@gavric.org>
2019-02-01 13:55:28 -0700

    Showcase single ads use

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 const app = express()
 const port = 3010
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname + '/views/index.html')))
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,33 @@
-alert('sup!')
+(function () {
+  // const BASE_URL = 'http://127.0.0.1:3000'
+  const BASE_URL = 'https://staging-my.shemedia.com'
+  const buttons = document.querySelectorAll('.element-selector')
+  const iframe = document.getElementById('single')
+  const EVENTS = {
+    SINGLE_ITEM_POSTED: 'SINGLE_ITEM_POSTED'
+  }
+
+  const handleSelectorClick = e => {
+    const selector = e.target.innerText
+
+    // pass isDebug parameter to inspect form state
+    iframe.src = BASE_URL +
+      '/boomerang-edit/single/?selector=' +
+      encodeURIComponent(selector)
+  }
+
+  const receiveMessage = e => {
+    const { data } = e
+
+    switch (data.type) {
+      case EVENTS.SINGLE_ITEM_POSTED:
+        console.log('handle saving to local storage here', data.payload)
+    }
+  }
+
+  buttons.forEach(button =>
+    button.addEventListener('click', handleSelectorClick)
+  )
+
+  window.addEventListener('message', receiveMessage, false)
+}())
